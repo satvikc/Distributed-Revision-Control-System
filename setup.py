f7ed88e39c2485e4b9c55732cc0c6259cd3a86d7
satvikc <mystic.satvik@gmail.com>
2012-10-03 08:22:54 +0530

    initial structure added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Devil',
+    version='0.1.0',
+    author='Satvik Chauhan',
+    author_email='mystic.satvik@gmail.com',
+    packages=['devil'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Devil/',
+    license='LICENSE.txt',
+    description='Distributed Version Control System',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
