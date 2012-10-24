be3b29533b8534c8657ce891acb7d0e9c2e6205a
rahulaaj <rahulajmera91@gmail.com>
2012-10-24 06:32:31 -0700

    latest

--- devil/file.py
+++ devil/file.py
@@ -1,11 +1,13 @@
+#! /usr/bin/python
 import exceptions,os,shutil,hashlib,datetime
+from optparse import OptionParser
 
 class FileController(object):
     """
     Class to perform all the local functionalities of the version
     control system.
     """
-    def __init__(self,directory):
+    def __init__(self):
         """
         Args:
           directory(str): Full path of the directory where the
@@ -13,22 +15,25 @@ class FileController(object):
         Returns:
           FileController Object
         """
-        self.directory = directory
-	self.username=''
+        self.directory = os.getcwd()
+	#self.username=''
 	#self.dictionary = []
-
+	
     def start(self):
         """
         Initiates the repository
         """
 	try:
-		os.makedirs(self.directory + '/devil')
+		os.makedirs(self.directory + '/Devil')
 	except OSError, e:
 	        if e.errno != errno.EEXIST:
         	    raise
-	username=raw_input("Enter username")
-	self.username=username
-        
+	username=raw_input("Enter username:\n")
+	uname=open(self.directory + '/Devil/'+'username.txt','w')
+	uname.write(username)
+	uname.close()
+	files=open(self.directory + '/Devil/'+'files.txt','w')
+	files.close()
 
     def add(self,filename):
         """
@@ -42,50 +47,93 @@ class FileController(object):
           FileOrDirectoryDoesNotExist : When the file or directory
           does not exist.
         """
-	if (os.path.isfile(filename) == True):
+	if (os.path.isfile(filename) == True or os.path.isdir(filename) == True):
 		#self.dictionary.append((os.path.abspath(filename),'notcommited'))
-		files=open('files.txt','U')
+		files=open(os.path.abspath('Devil/files.txt'),'U')
 		lines=files.readlines();
-		files=open('files.txt','w')
+		present=0
 		for line in lines:
 			path=line.split(" ")
 			if(path[0] == os.path.abspath(filename)):
-				files.write(path[0] + " " + "notcommited\n")
-			else:
+				present=1
+		if(present==0):
+			print "absent"
+			files=open(os.path.abspath('Devil/files.txt'),'w')
+			for line in lines:
 				files.write(line)
-				
+			files.write(os.path.abspath(filename) + " " + "notcommited\n")
+			
+		elif(present==1):
+			files=open(os.path.abspath('Devil/files.txt'),'w')
+			for line in lines:
+				path=line.split(" ")
+				if(path[0] == os.path.abspath(filename)):
+					files.write(path[0] + " " + "notcommited\n")
+				else:
+					files.write(line)
+    """				
 	elif (os.path.isdir(filename) == True):
 		for f in os.listdir(filename):
 			if (os.path.isfile(f) == True): 
 				#self.dictionary.append((os.path.abspath(f),'notcommited'))
-				files=open('files.txt','U')
+				files=open(os.path.abspath('Devil/files.txt'),'U')
 				lines=files.readlines();
-				files=open('files.txt','w')
+				present=0
 				for line in lines:
 					path=line.split(" ")
-					if(path[0] == os.path.abspath(filename)):
-						files.write(path[0] + " " + "notcommited\n")
-					else:
+					if(path[0] == os.path.abspath(filename + '/' + f)):
+						present=1
+				if(present==0):
+					print "first time"
+					files=open(os.path.abspath('Devil/files.txt'),'w')
+					for line in lines:
 						files.write(line)
-			elif (os.path.isdir(f) == True):
-				self.add(f)
+					files.write(os.path.abspath(filename + '/' + f) + " " + "notcommited\n")
+			
+				elif(present==1):
+					print "not first time"
+					files=open(os.path.abspath('Devil/files.txt'),'w')
+					for line in lines:
+						path=line.split(" ")
+						if(path[0] == os.path.abspath(filename + '/' + f)):
+							files.write(path[0] + " " + "notcommited\n")
+						else:
+							files.write(line)
+			elif (os.path.isdir(filename + '/' + f) == True):
+				print "adding subdirectory"
+				self.add(filename + '/' + f)
+
 	else:
-		raise FileOrDirectoryDoesNotExist()	
+		x=0#raise FileOrDirectoryDoesNotExist()	
+    """
 
     def commit(self,message):
 	#hashmap='lkfdsadf'
-	username=self.username
+	uname=open(self.directory + '/Devil/'+'username.txt','U')
+	username=str(uname.readlines())
+	uname.close()
 	dateandtime=str(datetime.datetime.now())
 	hashmap=hashlib.sha224(username + dateandtime).hexdigest()
-	files=open('files.txt','U')
+	files=open(os.path.abspath('Devil/files.txt'),'U')
 	lines=files.readlines();
+	#os.makedirs(os.path.abspath('Devil')+'/object'+'/'+hashmap)
 	for line in lines:
 		path=line.split(" ")
-		if(path[1]=="commited\n"):
-			shutil.copy2(path[0],os.path.abspath('devil')+'/object'+'/'+hashmap)
-	mfile=open(os.path.abspath('devil')+'/object'+'/'+hashmap+'/'+'message.txt','w')
+		if(path[1]=="notcommited\n"):
+			if(os.path.isfile(path[0])== True):
+				if not (os.path.exists(os.path.abspath('Devil')+'/object'+'/'+hashmap)):
+					os.makedirs(os.path.abspath('Devil')+'/object'+'/'+hashmap)
+				shutil.copy2(path[0],os.path.abspath('Devil')+'/object'+'/'+hashmap)
+			elif(os.path.isdir(path[0])== True):
+				shutil.copytree(path[0],os.path.abspath('Devil')+'/object'+'/'+hashmap)
+	mfile=open(os.path.abspath('Devil')+'/object'+'/'+hashmap+'/'+'message.txt','w')
 	mfile.write(message)
 	mfile.close()
+	files=open(os.path.abspath('Devil/files.txt'),'w')
+	for line in lines:
+		path=line.split(" ")
+		files.write(path[0] + " commited\n")
+	files.close()
 
     def rename(self,newname):
 	if os.path.exists(newname):
@@ -107,12 +155,12 @@ class FileController(object):
         pass
 
     def status(self):
-        files=open('files.txt','U')
+        files=open(os.path.abspath('Devil/files.txt'),'U')
 	lines=files.readlines();
 	for line in lines:
 		path=line.split(" ")
 		if (path[1]== "notcommited\n"):
-			print line
+			print "File	",path[0],"	",path[1]
 
     def pull(self,url):
         pass
@@ -122,3 +170,34 @@ class FileController(object):
 
     def revert(self,commit_hash):
         pass
+
+def main():
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser(usage)
+    parser.add_option("-i", "--init", help = "Initialize the repo", dest="init",action= "store_true")
+    parser.add_option("-a", "--add", help = "add files to tracking system", dest="add",action= "store")
+    parser.add_option("-c", "--commit",help="commit the required changes", dest="commit",action= "store")
+    parser.add_option("-s", "--status",help="all not commited files", dest="status",action= "store_true")
+    parser.add_option("-l", "--log",help="complete list of commits", dest="log",action= "store_true")
+    (options, args) = parser.parse_args()
+    if options.init:
+        #print("Initializing repo")
+	obj=FileController()
+	obj.start()
+    elif options.add:
+	obj=FileController()
+	#print options.add
+	obj.add(options.add)
+    elif options.commit:
+	obj=FileController()
+	obj.commit(options.commit)
+    elif options.status:
+	obj=FileController()
+	obj.status()
+    elif options.log:
+	obj=FileController()
+        obj.log()
+
+
+if __name__ == "__main__":
+    main()
