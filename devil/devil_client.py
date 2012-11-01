5580fa7b1ca03cca80959144b9263579d49aaaf8
rahulaaj <rahulajmera91@gmail.com>
2012-11-01 04:27:47 -0700

    merge working

--- devil/devil_client.py
+++ devil/devil_client.py
@@ -41,19 +41,19 @@ class DevilClient(pb.Root):
         obj=FileController(os.getcwd())
         obj.uncompressAndWrite(cont)
         common_commit=[x for x in commits if x in set(mycommits)]
-        print getLastCommit(common_commit),"\n"
+        #print getLastCommit(common_commit),"\n"
         #print (parent_commit.split()[1])
         parent_file_list=obj.getFileList(getLastCommit(common_commit))
         my_file_list=obj.getFileList(getLastCommit(mycommits))
-        print my_file_list,"\n"
+        #print my_file_list,"\n"
         other_file_list=obj.getFileList(getLastCommit(commits))
-        print other_file_list,"\n"
+        #print other_file_list,"\n"
         flag=0
         for elem in my_file_list:
                 for temp in other_file_list:
                         if(elem[0]==temp[0]):
                                 dicts=merge3.devilMerge(obj.getFile(getLastCommit(common_commit),elem[0]),obj.getFile(getLastCommit(mycommits),elem[0]),obj.getFile(getLastCommit(commits),elem[0]))
-                                print dicts
+                                #print dicts
                                 #reactor.stop()
                                 
                                 print "opening file ",elem[0]
